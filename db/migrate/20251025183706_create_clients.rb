ac9fcca6f954498ed47b91f21ba9dd960793ad5f
JavoMCL <hahusussu282828@gmail.com>
2025-10-25 18:49:33 -0300

    primer commit

---
+++ db/migrate/20251025183706_create_clients.rb
@@ -0,0 +1,13 @@
+class CreateClients < ActiveRecord::Migration[8.0]
+  def change
+    create_table :clients do |t|
+      t.string :name, null: false
+      t.string :type, null: false
+      t.string :phone_number
+      t.string :email
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
